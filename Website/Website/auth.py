1af101e8debcd9df2ebd99764966de8838aae044
yshrkume <yshrkume@gmail.com>
2024-08-16 14:28:10 +0900

    use auth service

--- Website/Website/auth.py
+++ Website/Website/auth.py
@@ -1,28 +1,42 @@
 #auth.py
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from . import db
-from .models import User
-from flask_login import login_user, logout_user, login_required, current_user
+from flask import Blueprint, render_template, redirect, url_for, request, flash, make_response
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity,
+    create_access_token,
+    set_access_cookies,
+    unset_jwt_cookies,
+)
+import requests
 # consider password hash
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 auth = Blueprint('auth', __name__)
+
+AUTH_SERVICE_URL = "http://127.0.0.1:5009"
 
 @auth.route("/login", methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get("email")
-        password = request.form.get("password1")
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Logged in sucessfully', category='success')
-            login_user(user, remember=True)
-            return redirect(url_for('views.home'))
+        response = requests.post(
+            f"{AUTH_SERVICE_URL}/login",
+            json={"username": username, "password": password},
+        )
+        if response.status_code == 200:
+            token = response.json().get("access_token")
+            resp = make_response(redirect(url_for("views.home")))
+            set_access_cookies(resp, token)
+            return resp
         else:
-            flash('Account does not exist', category='error')
+            flash("Invalid username or password", category="error")
+            return redirect(url_for("auth.login"))
 
-    return render_template("login.html", user = current_user)
+    return render_template("login.html")
+
 
 @auth.route("/sign-up", methods = ['GET', 'POST'])
 def signup():
@@ -32,35 +46,33 @@ def signup():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        existing_email = User.query.filter_by(email=email).first()
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_email:
-            flash('The email already exists', category='error')
-        elif existing_user:
-            flash('The username is already taken', category='error')
-        elif password1 != password2:
+        if password1 != password2:
             flash('Please type your password correctly', category='error')
         elif len(username) < 2:
             flash('Username must be more than 2 characters', category='error')
         elif len(password1) < 4:
             flash('Password must be more than 4 digits', category='error')
         else:
-            new_user = User(email=email, username=username, password = password1)
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember='True')
-            flash('Account Created')
-            return redirect(url_for("views.home"))
-        
-    return render_template("signup.html", user = current_user)
+            response = requests.post(
+                f"{AUTH_SERVICE_URL}/register",
+                json={
+                    "username": username,
+                    "email": email,
+                    "password": password1,
+                },
+            )
+            if response.status_code == 201:
+                flash("Congratulations, you are now a registered user!", category="success")
+                return redirect(url_for("views.home"))
+            else:
+                flash("Error registering user.", category="error")
+                return redirect(url_for("signup"))
 
-
-        
-
+    return render_template("signup.html")
 
 @auth.route("/logout")
-@login_required
 def logout():
-    logout_user()
-    flash('Logged out sucessfully', category='success')
-    return redirect(url_for("views.home"))
+    resp = make_response(redirect(url_for("views.home")))
+    unset_jwt_cookies(resp)
+    flash("Logout successful", category="success")
+    return resp
--- Website/Website/views.py
+++ Website/Website/views.py
@@ -1,10 +1,17 @@
 #views.py
 from flask import Blueprint, render_template, flash, request, redirect, url_for, jsonify
-from flask_login import current_user, login_required
-from .models import Post, User
+from .models import Post
 from . import db
 import requests
 from datetime import datetime
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity,
+    create_access_token,
+    set_access_cookies,
+    unset_jwt_cookies,
+    verify_jwt_in_request,
+)
 
 views = Blueprint('views', __name__)
 
@@ -13,7 +20,9 @@ MS_GR_URL = 'http://localhost:5003/greetAPI'
 
 @views.route("/")
 @views.route("/home")
+@jwt_required(optional=True, locations=["cookies"])
 def home():
+    user = get_jwt_identity()
     '''response = requests.get(MS_GR_URL)
     greeting_response = response.json()'''
     try:
@@ -26,70 +35,78 @@ def home():
     except Exception as e:
         flash(f'Notification failed: {e}', category='error')
     posts = Post.query.all()
-    return render_template("home.html", user=current_user, posts=posts)
+    return render_template("home.html", user=user, posts=posts)
 
 # post upload
+@jwt_required(locations=["cookies"])
 @views.route("/create-post", methods=['GET', 'POST'])
-@login_required
 def create_post():
-
+    verify_jwt_in_request()
+    user = get_jwt_identity()
     if request.method == "POST":
         text = request.form.get('text')
 
         if not text:
             flash('Text entry is empty', category='error')
         else:
-            post = Post(text=text, author=current_user.id)
+            post = Post(text=text, author_id=user["id"])
             db.session.add(post)
             db.session.commit()
             # notification microservice
-            notify_request(current_user.username, 'created')
+            notify_request(user["username"], 'created')
             flash('Post created!', category='success')
             return redirect(url_for("views.home"))
 
-    return render_template("create_post.html", user=current_user)
+    return render_template("create_post.html", user=user)
 
 # post upload-2
+@jwt_required(locations=["cookies"])
 @views.route("/create-post-2", methods=['GET', 'POST'])
-@login_required
 def create_post_2():
-
+    verify_jwt_in_request()
+    user = get_jwt_identity()
     if request.method == "POST":
         text = request.form.get('text')
 
         if not text:
             flash('Text entry is empty', category='error')
         else:
-            post = Post(text=text, author=current_user.id)
+            post = Post(text=text, author_id=user["id"])
             db.session.add(post)
             db.session.commit()
             # notification microservice
-            notify_request(current_user.username, 'created')
+            notify_request(user["username"], 'created')
             flash('Post created!', category='success')
             return redirect(url_for("views.home"))
 
-    return render_template("create_post.html", user=current_user)
+    return render_template("create_post.html", user=user)
 
 # user post list
+@jwt_required(locations=["cookies"])
 @views.route("/posts/<username>")
 def posts(username):
-    user = User.query.filter_by(username=username).first()
-    posts = Post.query.filter_by(author=user.id).all()
-    return render_template("user_posts.html", user=current_user, posts=posts, username=username)
+    verify_jwt_in_request()
+    user = get_jwt_identity()
+    posts = Post.query.filter_by(author_id=user["id"]).all()
+    return render_template("user_posts.html", user=user, posts=posts, username=username)
 
 # post delete
+@jwt_required(locations=["cookies"])
 @views.route("/delete/<id>")
-@login_required
 def delete_post(id):
+    verify_jwt_in_request()
+    user = get_jwt_identity()
     post = Post.query.filter_by(id=id).first()
     db.session.delete(post)
     db.session.commit()
     # notification microservice
-    notify_request(current_user.username, 'deleted')
+    notify_request(user["username"], 'deleted')
     flash('Post deleted.', category='success')
     return redirect(url_for('views.home'))
 
+@jwt_required(locations=["cookies"])
 def notify_request(user, action):
+    verify_jwt_in_request()
     try:
         response = requests.post(MS_PN_URL, json={'user': user, 'action': action})
         if response.status_code != 200:
